52afbf8bfee9799a014ea1e018f53eefb558beb6
tachukao <tachu.kao@gmail.com>
2021-12-10 17:52:46 +0800

    change antv3 to ant_v3

--- examples/ant.ml
+++ examples/ant.ml
@@ -2,13 +2,13 @@ open Mujoco
 open Mujoco_env
 
 let () =
-  let env = Antv3.init () in
+  let env = Ant_v3.init () in
   Owl.Arr.print Model.(ctrlrange env.model);
   let terminated = ref false in
   Printf.printf "start\n%!";
   while not !terminated do
     let action = Owl.Arr.uniform ~a:(-0.01) ~b:0.01 [| Model.nu env.model |] in
-    let _, r, term = Antv3.(step env action) in
+    let _, r, term = Ant_v3.(step env action) in
     Printf.printf "%f\n%!" r;
     terminated := term
   done
